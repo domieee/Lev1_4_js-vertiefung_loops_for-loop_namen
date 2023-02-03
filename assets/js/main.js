207e217ed99d98de7666647ecca1e9adc04e3226
domieee <99103235+domieee@users.noreply.github.com>
2023-02-03 12:30:53 +0100

    added js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let employees = ["Freddy", "Steffen", "Finn", "Julia", "Franzi", "Christian", "Sergio"]
+
+// Deklariert 'let i = 1' als Zählvariable
+// Pro Iteration gibt die Konsole das Element aus, dessen Indexnummer 'i' entspricht
+
+for(let i = 0; i != employees.length; i++) {
+    console.log(employees[i]);
+}
